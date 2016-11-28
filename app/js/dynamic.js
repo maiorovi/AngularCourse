6c99d8b1a43f694a857ba6a7028ad0b6dfa0db3a
DSK13 DSK <DSK13@ptc.local>
2016-11-28 15:54:49 +0200

    initial commit

---
+++ app/js/dynamic.js
@@ -0,0 +1,20 @@
+var phonecatApp = angular.module('phoneList', []);
+
+
+phonecatApp.controller('PhoneListController', function ($scope) {
+  $scope.phones = [
+    {
+     name: 'Nexus S',
+     snippet: 'Fast just got faster with Nexus S.'
+   }, {
+     name: 'Motorola XOOM™ with Wi-Fi',
+     snippet: 'The Next, Next Generation tablet.'
+   }, {
+     name: 'MOTOROLA XOOM™',
+     snippet: 'The Next, Next Generation tablet.'
+   },  {
+     name:'Samsung Galaxy 7',
+     snippet: 'This is very cool phone.'
+   }
+  ]
+});
---
+++ app/js/phonecatApp.module.js
@@ -0,0 +1,1 @@
+var phonecatApp = angualr.module('phonecatApp', ['phoneList'])
